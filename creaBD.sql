25f32c816613db4b562a23ac205c83d09982c309
Gabriel Garcia <gabriel.garcia@estudiants.urv.cat>
2022-05-29 17:18:47 +0200

    Consultas finalizadas

--- creaBD.sql
+++ creaBD.sql
@@ -58,7 +58,6 @@ create table armes_biologiques(
     potencial tinyint(2) not null,
     zona int(5) not null unique,
     lab int(5) not null,
-    -- add check for potencial
     constraint ck_potencial CHECK (potencial >=1 AND potencial<=10),
     constraint pk_armes_biologiques primary key (nom),
     constraint fk_armes_biologiques_zones_biocontencio foreign key (zona,lab) references zones_biocontencio(codi,codiLab)
@@ -80,26 +79,28 @@ insert into paisos(nom,pot_desenv,tractat_signat) values ('Espanya',10,'S'); -- 
 insert into laboratoris(codi,nom,pais) values (00001, 'Lab01','Espanya');
 insert into empleats(num_pass,nom) values ('12345','Gabriel');
 insert into qualificats(num_pass,titulacio,zona_assignada,lab)values('12345','Medicina',null,null);
-insert into zones_biocontencio(codi,codiLab,nivell,responsable) values (00000,00001,'A','12345');--check for ck_nivell
+insert into zones_biocontencio(codi,codiLab,nivell,responsable) values (00000,00001,'M','12345');--check for ck_nivell
 update qualificats set zona_assignada=00000, lab = 00001 where num_pass = '12345';
 insert into armes_biologiques(nom,fecha,potencial,zona,lab) values ('Arma1','2022-05-28',8,00000,00001); --check for potencial
 
 --3
+/*
 select l.codi,l.nom 
 from laboratoris as l, zones_biocontencio as z 
 where z.codiLab= l.codi AND z.nivell='A'
 order by l.nom asc;
-
+*/
 --4
 insert into empleats(num_pass,nom) values('23456','Josep');
 insert into ordinaris(num_pass) values ('23456');
 insert into assignacions(fecha,empl_ord,zona,lab,data_fi) values ('2022-05-20','23456',00000,00001,'2022-05-28');
+/*
 select e.nom 
 from empleats as e,ordinaris as o
 where e.num_pass=o.num_pass AND o.num_pass not in (select a.empl_ord 
                             from assignacions as a,zones_biocontencio as z 
                             where a.zona = z.codi AND z.nivell='A');
-
+*/
 --5
 insert into empleats(num_pass,nom) values ('34567','Maria');
 insert into qualificats(num_pass,titulacio,zona_assignada,lab)values('34567','Biologia',00000,00001);
@@ -107,16 +108,61 @@ insert into empleats(num_pass,nom) values ('45678','Sonia');
 insert into qualificats(num_pass,titulacio,zona_assignada,lab)values('45678','Microbiologia',00000,00001);
 insert into empleats(num_pass,nom) values ('56789','Alex');
 insert into qualificats(num_pass,titulacio,zona_assignada,lab)values('56789','Quimica',00000,00001);
+/*
 select z.codi,l.nom
 from zones_biocontencio as z, laboratoris as l
 where z.codiLab=l.codi AND 3<  (select COUNT(q.zona_assignada)
                                 from qualificats as q
                                 where z.codi=q.zona_assignada and z.codiLab=q.lab)
 order by l.codi, z.codi;
+*/
 
---6
-select o.*
-from ordinaris as o,assignacions as a
-where o.num_pass = a.empl_ord and o.num_pass in (select a.empl_ord 
-                                                     from laboratoris as l,zones_biocontencio as z
-                                                     where l.codi = z.codiLab and l.nom='BCN-XXX');
+insert into laboratoris(codi,nom,pais) values (00002, 'BCN-XXX','Espanya');
+insert into empleats(num_pass,nom) values ('67891','Arnau');
+insert into qualificats(num_pass,titulacio,zona_assignada,lab)values('67891','Fisica',null,null);
+insert into zones_biocontencio(codi,codiLab,nivell,responsable) values (00001,00002,'B','67891');
+update qualificats set zona_assignada=00001, lab = 00002 where num_pass = '67891';
+insert into empleats(num_pass,nom) values ('78912','Manel');
+insert into qualificats(num_pass,titulacio,zona_assignada,lab)values('78912','Matematiques',null,null);
+insert into zones_biocontencio(codi,codiLab,nivell,responsable) values (00002,00002,'M','78912');
+update qualificats set zona_assignada=00002, lab = 00002 where num_pass = '78912';
+insert into empleats(num_pass,nom) values ('89123','Kilian');
+insert into qualificats(num_pass,titulacio,zona_assignada,lab)values('89123','Estadistica',null,null);
+insert into zones_biocontencio(codi,codiLab,nivell,responsable) values (00003,00002,'M','89123');
+update qualificats set zona_assignada=00003, lab = 00002 where num_pass = '89123';
+
+insert into empleats(num_pass,nom) values('91234','Aaron');
+insert into ordinaris(num_pass) values ('91234');
+insert into assignacions(fecha,empl_ord,zona,lab,data_fi) values ('2022-05-20','91234',00001,00002,'2022-05-29');
+insert into assignacions(fecha,empl_ord,zona,lab,data_fi) values ('2022-05-21','91234',00002,00002,'2022-05-29');
+insert into assignacions(fecha,empl_ord,zona,lab,data_fi) values ('2022-05-22','91234',00003,00002,'2022-05-29');
+
+
+
+
+insert into empleats(num_pass,nom) values('00012','Jordi Guasch');
+insert into ordinaris(num_pass) values ('00012');
+insert into assignacions(fecha,empl_ord,zona,lab,data_fi) values ('2022-05-20','00012',00001,00002,'2022-05-29');
+insert into assignacions(fecha,empl_ord,zona,lab,data_fi) values ('2022-05-21','00012',00002,00002,'2022-05-29');
+
+
+
+--9
+update zones_biocontencio,armes_biologiques
+set zones_biocontencio.nivell='A'
+where armes_biologiques.zona=zones_biocontencio.codi and armes_biologiques.potencial>5;
+
+select *
+from zones_biocontencio
+where zones_biocontencio.codi = 00000;
+
+
+--10
+alter table zones_biocontencio modify responsable varchar(9) null;
+update qualificats,zones_biocontencio
+set qualificats.zona_assignada=zones_biocontencio.codi, zones_biocontencio.responsable=null
+where qualificats.num_pass=zones_biocontencio.responsable and qualificats.lab = zones_biocontencio.codiLab;
+
+
+select *
+from zones_biocontencio;
